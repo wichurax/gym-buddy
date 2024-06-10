ae8e19d0892137c3f72b7fd7e03af20ab891115c
wichurax <matsowinskiw@gmail.com>
2024-06-11 00:48:02 +0200

    fixed npm build

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Test from './components/ui/test'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -29,9 +28,6 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-      <div>
-        <Test/>
-      </div>
     </>
   )
 }
